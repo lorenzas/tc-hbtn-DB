5e142753806f4191f279c2d5be40bf8c861e3500
lorenzas <lorenza@ciandt.com>
2022-06-27 10:18:35 -0300

    Initial commit

---
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -0,0 +1,14 @@
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class PrintJDBCDrivers {
+    public static void main(String[] args) {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+
+            System.out.printf("Nome: %s Versão: %d\n", driver.getClass().getName(), driver.getMajorVersion());
+        }
+    }
+}
